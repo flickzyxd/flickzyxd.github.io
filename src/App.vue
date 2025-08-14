4b85b99c430301c0406a4d909a18c9244687a470
flickzyxd <stepanfominykh2000@gmail.com>
2025-08-15 01:42:46 +0300

    trying to change the future

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from "./components/AppHeader.vue";
+</script>
+
+<template>
+  <HelloWorld/>
+  <AppHeader></AppHeader>
+  <div> asdasd</div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+asDasdasaasdasdas
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+
+</style>
