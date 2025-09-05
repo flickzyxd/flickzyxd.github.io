1a31a28cae7f5c39a342e476a2a51aa0608daaca
flickzyxd <stepanfominykh2000@gmail.com>
2025-09-05 09:41:17 +0300

    .nojekyll

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -37,5 +36,7 @@ export default defineConfig({
   preview: {
     port: 4173,
     host: true
-  }
+  },
+
+  base: './'
 })
