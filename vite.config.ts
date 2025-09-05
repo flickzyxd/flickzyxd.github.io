8442b1dab7280c4b0f8bb18445e1e6555664e6b0
flickzyxd <stepanfominykh2000@gmail.com>
2025-09-05 12:33:38 +0300

    adding dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: './deploying-vite-project/',
   plugins: [
     vue(),
     {
@@ -38,5 +39,4 @@ export default defineConfig({
     host: true
   },
 
-  base: './'
 })
